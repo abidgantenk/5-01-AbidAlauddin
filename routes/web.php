75dd71ecbce094d6dc8261ef60c2b5a4598b9c50
abidgantenk <3103120002@student.smktelkom-pwt.sch.id@gmail.com>
2022-04-19 16:07:01 +0700

    modul 7

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
 
 Route::get('/', function (){
-    return view ('home',[
-        "title" => "Home"
+    return view ('index',[
+        "title" => "Beranda"
     ]);
 });
 
@@ -24,4 +24,19 @@ Route::get('/gallery', function (){
     ]);
 });
 
-Route::resource('/contacts', ContactController::class);
+//Route::resource('/contacts', ContactController::class);
+Route::get('/contacts/create', [ContactController::class, 'create'])->name('contacts.create');
+Route::post('/contacts/store', [ContactController::class, 'store'])->name('contacts.store');
+
+Auth::routes();
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/contacts/index', [ContactController::class, 'index'])->name('contacts.index');
+    Route::get('/contacts/{id}/edit', [ContactController::class, 'edit'])->name('contacts.edit');
+    Route::post('/contacts/{id}/update', [ContactController::class, 'update'])->name('contacts.update');
+    Route::get('/contacts/{id}/destroy', [ContactController::class, 'destroy'])->name('contacts.destroy');
+});
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
