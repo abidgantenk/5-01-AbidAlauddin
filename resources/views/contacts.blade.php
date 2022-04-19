0f23b2be8421d0521a9d77dc075476b63815e62a
abidgantenk <3103120002@student.smktelkom-pwt.sch.id@gmail.com>
2022-04-19 16:32:52 +0700

    modul 7

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -1,22 +1,23 @@
 @extends('layouts.main')
 
 @section('container')
-<div classs="title-top mb-5">
-    <h2 class="fw-bold">Contact Us</h2>
-</div>
-<form>
-  <div class="form-floating mb-3">
-    <input type="text" class="form-control" id="inputname" placeholder="Nama">
-    <label for="inputname">Nama</label>
-  </div>
-  <div class="form-floating mb-3">
-    <input type="email" class="form-control" id="inputemail" placeholder="Email">
-    <label for="inputname">Nama</label>
-  </div>
-  <div class="form-floating mb-3">
-    <textarea class="form-control" id="floatingTextarea2" style="height: 100px" placeholder="Tinggalkan pesan disini"></textarea>
-    <label for="floatingTextarea2">Pesan</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+        <div class="title-top mb-3">
+            <h2 class="fw-bold">Contacts Us</h2>
+        </div>
+        <form method="post" action="{{route('contacts.store')}}">
+            {{csrf_field()}}
+            <div class="form-floating mb-3">
+                <input type="text" class="form-control" id="inputname" placeholder="Nama" name="nama">
+                <label for="inputname">Nama</label>
+            </div>
+            <div class="form-floating mb-3">
+                <input type="email" class="form-control" id="inputemail" placeholder="Email" name="email"> 
+                <label for="inputemail" class="form-label">Email</label>
+            </div>
+            <div class="form-floating mb-3">
+                <textarea class="form-control" placeholder="Tinggalkan Pesan Disini" id="floatingTextarea2" style="height: 100px" name="pesan"></textarea>
+                <label for="floatingTextarea2">Pesan</label>
+            </div>
+            <button type="submit" class="btn btn-primary">Kirim</button>
+        </form>
 @endsection
